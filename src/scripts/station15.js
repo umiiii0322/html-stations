c78e952a149837abf5ce1f6fd4f67ff58d8b662c
umiiii0322 <unno.shota1222@gmail.com>
2024-06-30 23:19:02 +0900

    1-15 途中まで

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,3 +1,5 @@
+const { map } = require("fp-ts/lib/Functor");
+
 async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
@@ -6,11 +8,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(data) {
+  new Promise((resolve) =>{
+    setTimeout(() =>{
+      console.log("3秒経過");
+      data.full_name = `${data.family_name} ${data.first_name}`;
+      console.log(data);
+      resolve();
+    },3000);
+  }).then(() =>{
+    return data;
+  });
+  
 }
-
